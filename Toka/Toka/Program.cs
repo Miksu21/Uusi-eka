1c94d0ce40cb8b98e9f694e1fae5aae0837703be
Miksu21 <mikaahonen21@gmail.com>
2023-03-24 09:13:30 +0200

    uusi toka

---
+++ Toka/Toka/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// Harjoitus
+            string message = "Hello";
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            Console.WriteLine("Vastasit" + input);
+
+
+
+
+
+
+
+        }
+    }
+}
